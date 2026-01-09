4bfc9b7ae2724adeffa6794ff09c55d426676d62
ViswanadhBolla <viswanadth1234@gmail.com>
2026-01-10 00:46:48 +0530

    Add application layer for catalog service with commands, queries, and mapping

---
+++ Services/Catalog/Catalog.Application/Handlers/GetAllProductsHandler.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Catalog.Application.Handlers;
+
+public class GetAllProductsHandler
+{
+
+}
---
+++ Services/Catalog/Catalog.Application/Mappers/ProductMapper.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Catalog.Application.Mappers;
+
+public static class ProductMapper
+{
+    private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.ShouldMapProperty = p => p.GetMethod!.IsPublic || p.GetMethod!.IsAssembly;
+            cfg.AddProfile<ProductMappingProfile>();
+        }, NullLoggerFactory.Instance);
+        return config.CreateMapper();
+    });
+
+    public static IMapper Mapper => Lazy.Value;
+}
